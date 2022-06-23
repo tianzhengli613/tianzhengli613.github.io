44abf04158cd32bef7848e51698a593265dde3fa
Tianzheng Li <tianzhengli613@gmail.com>
2022-06-23 10:48:10 -0700

    working gallery

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Navbar
-function navSlide() {
+function navbar() {
 	const burger = document.querySelector(".burger");
-	const nav = document.querySelector(".nav-links");
-	const navLinks = document.querySelectorAll(".nav-links li");
+	const nav_links = document.querySelector(".nav-links");
+	const nav_item = document.querySelectorAll(".nav-links li");
 	
 	burger.addEventListener("click", () => {
-		// Toggle Nav
-		nav.classList.toggle("nav-active");
+		// toggle .nav-links
+		nav_links.classList.toggle("nav-active");
 		
-		// Link fade animation
-		navLinks.forEach((link, index) => {
+		// .nav-links animation
+		nav_item.forEach((link, index) => {
 			if (link.style.animation) {
 				link.style.animation = ""
 			} else {
@@ -17,30 +17,10 @@ function navSlide() {
 			}
 		});
 		
-		// Burger toggle animation
+		// burger animation
 		burger.classList.toggle("toggle");
 	});
 	
 }
 
-navSlide();
-
-
-// Carousel
-const buttons = document.querySelectorAll("[data-carousel-button]")
-buttons.forEach(button => {
-	button.addEventListener("click", () => {
-		const offset = button.dataset.carouselButton === "next" ? 1 : -1
-		const slides = button
-			.closest("[data-carousel]")
-			.querySelector("[data-slides]")
-
-		const activeSlide = slides.querySelector("[data-active]")
-		let newIndex = [...slides.children].indexOf(activeSlide) + offset
-		if (newIndex < 0) newIndex = slides.children.length - 1
-		if (newIndex >= slides.children.length) newIndex = 0
-
-		slides.children[newIndex].dataset.active = true
-		delete activeSlide.dataset.active
-	})
-})
+navbar();
